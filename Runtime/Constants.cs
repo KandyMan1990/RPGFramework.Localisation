d5f7b29f86fbc5f0fd5df57b2e8bc0f198844b64
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2025-12-10 21:40:40 +0000

    Update magic const to be consistent on read and write

--- Runtime/Constants.cs
+++ Runtime/Constants.cs
@@ -2,6 +2,17 @@
 {
     public static class Constants
     {
-        public const string MAGIC = "LOCB";
+        private const byte MAGIC_0 = (byte)'L';
+        private const byte MAGIC_1 = (byte)'O';
+        private const byte MAGIC_2 = (byte)'C';
+        private const byte MAGIC_3 = (byte)'B';
+
+        public static readonly byte[] MAGIC =
+        {
+                MAGIC_0,
+                MAGIC_1,
+                MAGIC_2,
+                MAGIC_3
+        };
     }
 }
