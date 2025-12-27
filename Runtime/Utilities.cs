45a3d3044ad4e6703daf54540c97001a0af3da3e
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2025-12-27 23:32:33 +0000

    Add Utilities helper

---
+++ Runtime/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace RPGFramework.Localisation
+{
+    internal static class Utilities
+    {
+        internal static string Combine(params string[] parts)
+        {
+#if UNITY_ANDROID || UNITY_WEBGL
+            return string.Join('/', parts.Select(p => p.Trim('/')));
+#else
+            return Path.Combine(parts);
+#endif
+        }
+
+        internal static string GetNeutralLanguage(string language)
+        {
+            int index = language.IndexOfAny(new[]
+                                            {
+                                                    '-',
+                                                    '_'
+                                            });
+
+            return index > 0 ? language[..index] : language;
+        }
+    }
+}
